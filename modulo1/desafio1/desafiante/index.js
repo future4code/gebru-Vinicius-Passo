039852a50457b801a9d8fa0de0e3b7804dae13c8
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2021-11-09 18:50:06 -0300

    Resolvi-o-Desafio-de-variantes

---
+++ modulo1/desafio1/desafiante/index.js
@@ -0,0 +1,14 @@
+//Desafio 1 Adição
+
+/*let n1 = Number(prompt('Digite um valor: '))
+let n2 = Number(prompt('Digite um outro valor: '))
+let s = n1 + n2
+console.log("A soma entre", n1, "e o", n2, "é igual", s)*/
+
+ //Desafio 2 Multiplicação
+
+
+ /*let n1 = Number(prompt('Adicione um número:'))
+ let n2 = Number(prompt('Adicione um outro valor: '))
+ let s = n1 * n2
+ console.log("A Multiplicação entre", n1, "e o", n2, "é igual", s)*/
