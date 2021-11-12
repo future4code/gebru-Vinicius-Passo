a618cbb3def43c9eb80707942af30c229c3d3357
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2021-11-11 21:16:53 -0300

    Resolvi-exercício-de-operadores

---
+++ modulo1/operadores/operando/index.js
@@ -0,0 +1,92 @@
+// EXERCÍCIO 1
+
+/*const bool1 = ture 
+const boo2 = false
+const bool3 = !bool2 */// bool3 é true, por ta falando que não é false
+
+/*let resultado = bool1 && bool2
+console.log("a. ", resultado) */ // vai imprimir false. no caso desse ex para imprimir verdadeiro os dois tiriam que ser verdadeiro!
+
+/*resultado = bool1 && bool2 bool3 
+console.log("b. ",resultado)*/ // vai imprimir false! 
+
+/*resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) */ //imprimirá false
+
+/*console.log("d. ", typeof resultado)*/ // vai informar que esse "d" é o do tipo instring
+
+// EXERCÍCIO 2
+
+/*let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)*/ // O primeiroNumero está fazendo uma concatenação com o segundoNumero, por seres do tipo string. pra resolver esse problema teria que usar o tipo Number!
+
+// EXERCÍCIO
+ 
+/*let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) *///acho que isso resolveria o problema do meu amigo
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO 1
+
+/*const idadeUsuario = Number(prompt('Qual a sua idade?'))
+const idadeMelhorAmigo = Number(prompt('Qual a idade do seu melhor amigo?'))
+
+    maiorIdade = idadeUsuario > idadeMelhorAmigo
+
+    diferenteIdade = idadeUsuario - idadeMelhorAmigo
+
+console.log(maiorIdade, diferenteIdade)*/ // Se a idade do usuário for maior que a do amigo imprimirá true, se a do amigo for maior retornará false, e logo ao lodo vai imprimir a diferença da idade deles!
+
+// EXERCÍCIO 3
+
+/*const numeroPar = Number(prompt("Digite um número par"))
+console.log(numeroPar % 2)*/ // como os número são o par o resto da divisão sera zero. se fosse número impar imprimeria qualquer numero que tenha sobrado depois do ponto. ex se fosse o numero 31 o resto da divisão seria 1
+
+// EXERCÍCIO 4
+
+/*const idade = Number (prompt('Digite sua idade em anos:'))
+    idadeEmMeses = 12 * idade
+    idadeEmDias = 365 * idade
+    idadeEmHoras = 8760 * idade
+
+    console.log(idadeEmMeses, 'meses de vida', idadeEmDias, 'dias de vida' ,idadeEmHoras ,'horas de vida')*/
+
+
+// EXERCÍCIO 4
+
+/*let n1 = Number(prompt('Digite um valor:'))
+let n2 = Number (prompt('Digite um outro valor:'))
+
+    res1 = n1 > n2
+    res2 = n1 === n2
+    res3 = n1 % n2 
+    res3 = 0 === res3 
+    res4 = n2 % n1
+    res4 = 0 === res4
+console.log(res1,res2,res3,res4)*/ 
+
+// DESAFIO
+// QUESTÃO1
+//a)
+
+  /* let f_k = Number(prompt('Para saber a temperatura em em kelvin, digite a temperatura em Fahrenheit:'))
+ res = 5 * (f_k - 32) / (9) + 273
+    console.log(res, '°F')*/
+//b)
+
+/*let c_f = Number(prompt('Digite a temperatura em grou celsius para saber em Fahrenheit:')) 
+res1 = (c_f / 5) * 9 + 32
+console.log(res1, '°F')*/
+ //nãoo terminei o desafio pq fiquei com medo de passar o tempo da entrega, mas vou continuar tentendo resolver o desafio
+
+
+
+
+   
