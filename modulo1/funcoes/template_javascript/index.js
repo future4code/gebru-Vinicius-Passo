24d2c40024d9957b5eafb61770d84bd5cda0c44f
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2021-11-19 22:42:07 -0300

    Exercício-de-funcoes

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,110 @@
+//EXERCÍCIO 1. DE ESCRITA DE CÓDIGO
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))*/
+
+// a) 
+/* vai imprimir no console dois resultados
+ no primeiro console.log vai mostrar o resultado 10
+ no segundo console.log vai mostrar o resultado 50*/
+
+ // b)
+ /* Se retirar os dois console.log, primeiro que não mostraria
+ os resultados no meu console. e se simplesmente invocasse 
+ a função minhaFuncao (2) e minhaFuncao(10) não mostraria no meu
+ console. porém estaria fazendo o calcule do mesmo jeito
+ e se eu quiser ver como estaria funcionando
+ daria um console.log para ver se estaria tudo bem.*/
+
+ // EXERCÍCIO 2 DE ESCRITA DE CÓDIGO
+
+ /*let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+
+// a)
+    /* Essa função vai retornar o texto do usuario todo 
+    em minúsculo mesmo ele colando algumas palavras em 
+    maiúsculo. em outras palavras mesmo se o texto estiver
+    todo em maiúsculo vai retornar minúsculo. E também 
+    vai retornar true se houver alguma escrita com a palavra
+    cenoura, não havendo essa palavra no texto, retornara false. */
+
+//b)
+    /* i. eu gosto de cenoura./ true
+    ii. cenoura é bom pra vista. /true
+    ii cenouras crescem na terra. /true*/
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO
+//1.
+    //a)
+   /* function minhasInformações(){
+      console.log("Eu sou vinicius, tenho 22 anos, moro em Campo Alegre de Lourdes BA, e sou estudante." )
+    }
+  minhasInformações()*/
+
+  //b)
+  /* function dadosPessoais () {
+        const nome = 'vinicius'
+        const idade = 22
+        const endereco = 'Campo Alegre de Lourdes'
+        const profissao = 'estudante'
+console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${profissao}.`)
+ }
+dadosPessoais()*/
+
+//EXERCÍCIO 2 DE ESCRITA DE CÓDIGO
+//a)
+   /* function somar(num1, num2) {
+       const soma =  num1 + num2
+        console.log(soma) 
+    }
+    somar(2,4)*/
+//b)
+    /*function comparandoValores (numero1, numero2) {
+        const comparador = numero1 >= numero2
+        console.log(comparador)
+    }
+    comparandoValores(2,4)*/
+//c)
+  /*  function parOuNão () {
+const parOuimpar = prompt('Digite um valor')
+const prova = parOuimpar % 2
+ console.log(prova === 0, "é par")
+ console.log( prova !== 0, "é impar!")
+
+return prova
+    }
+    parOuNão(4)*/
+//d)
+  /*  function tipoString(Menssagem ) {
+ console.log(Menssagem.toUpperCase())
+ console.log(Menssagem.length)
+ 
+    }
+    tipoString('vini')*/
+
+// EXERCÍCIO 3
+
+
+  /*  function somar(n1, n2){
+     
+      const somar = n1 + n2
+      const menos = n1 - n2
+      const multiplicar = n1 * n2
+      const dividir = n1 / n2 
+console.log(somar)
+console.log(menos)
+console.log(multiplicar)
+console.log(dividir.toFixed(1))
+    }
+   console.log(somar(4,50))*/
