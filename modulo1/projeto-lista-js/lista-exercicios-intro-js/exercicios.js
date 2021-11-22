89bbdafd41db1a9e8f1817d61d2e6dc240fbcc53
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2021-11-21 21:26:54 -0300

    atualizando-projeto-lista-js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -88,31 +88,65 @@ return array[0]
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
+  return array.pop()
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-
+let array1 = array.slice()
+array1[array.length -1] = array[0]
+array1 [0] = array[array.length -1]
+return array1 
 }
+
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
- return string1 <= string2
+ return (string1 <= string2)  , string1 !== string2
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+const anoAtual = Number(prompt('Qual é o ano atual?'))
+const anoNascimento = Number(prompt('Qual o ano do seu nascimento?'))
+const anoCarteiraEmitida = Number(prompt('Qual foi o ano em que sua carteira de identidade foi emitida?'))
+
+const idade = anoAtual - anoNascimento
+
+const checandoValidadeEmitida = anoAtual - anoCarteiraEmitida
+const idade = anoAtual - anoNascimento 
+
+const validadeRg = anoAtual - anoDeEmissaoRg 
+
+const condicao1 = idade <= 20 || validadeRg > 5
+
+const condicao2 = idade > 21 || idade <= 30 || validadeRg >= 10
+
+const condicao3 = idade > validadeRg >= 15
+
+ if (idade <= 20 && condicao1 >= 5) {
+     console.log(condicao1)
+
+ } else if(idade > 21 || idade <= 30 ){
+     console.log(condicao2)
+
+ } else if(condicao3 && idade  ) { 
+    
+     console.log(condicao3)
+    }
 
 }
+// tentei resolver esse desafio de checar a validade do rg, mas não consegui
+
+
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
-
+return ano%400 ==0 || ano %4 ==00 && ano %100 != 0
 }
 
 // EXERCÍCIO 15
