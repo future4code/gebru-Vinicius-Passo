bc61d7a15aec0a8bb9eb7b563431633b25fc1e85
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2021-12-02 11:39:15 -0300

    variaveis

---
+++ modulo1/variaveis-callback/variaveis/index.js
@@ -0,0 +1,29 @@
+// ------ EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO 1 ----
+
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" }
+//   ]
+  // a)
+//   const novoArrayA = usuarios.map((item, index,array) => {
+//      console.log(item, index, array)
+//   }) //Vai ser impresso tudo que estar dentro do array
+    // usuarios. da chaves nome, e chaves apelido
+
+    //---- EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO 1 -----
+
+    // const usuarios = [
+    //     { nome: "Amanda Rangel", apelido: "Mandi" },
+    //     { nome: "Laís Petra", apelido: "Laura" },
+    //     { nome: "Letícia Chijo", apelido: "Chijo" },
+    //   ]
+      
+    //   const novoArrayC = usuarios.filter((item, index, array) => {
+    //      return item.apelido !== "Chijo"
+    //   })
+      
+    //   console.log(novoArrayC) // vai imprimir da variavel usuario
+      // as chaves que for diferente de Chijo, ou seja na chave que tiver o nome Chinjo não sera impresso
+
+   
