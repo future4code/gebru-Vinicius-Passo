d992233331ac4ef3f20348c791332a901f15f2ea
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-03-17 19:12:02 -0300

    subindo-userEffect

--- modulo4/useEffect/use-effect/src/App.js
+++ modulo4/useEffect/use-effect/src/App.js
@@ -10,8 +10,8 @@ function App() {
   useEffect( () => {
     axios 
     .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-    .then( response => {
-      setPokeList(response.data.results)
+    .then( resp => {
+      setPokeList(resp.data.results)
     })
     .catch( err => {
       console.log(err)
