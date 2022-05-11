f7fe05ce110ba014f2907692b06d278d7e0a7c03
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-05-10 22:03:13 -0300

    test-javaScript

---
+++ modulo4/test/src/exercicios/ex2.test.js
@@ -0,0 +1,23 @@
+import { checaPalindromo } from "./ex2";
+
+describe("Checa Palíndromo", () => {
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorna true para 'arara'", () => {
+    const ehPalindromo = checaPalindromo("arara");
+    expect(ehPalindromo).toEqual(true);
+  })
+
+  it("retorna true para 'asa'", () => {
+    const ehPalindromo = checaPalindromo("asa");
+    expect(ehPalindromo).toEqual(true);
+  })
+
+  it("retorna false para 'marrocos'", () => {
+    const ehPalindromo = checaPalindromo("marrocos");
+    expect(ehPalindromo).toEqual(false);
+  })
+});
