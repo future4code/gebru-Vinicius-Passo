546c01247b432959c32b873829b5be75995d266d
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-05-19 19:24:14 -0300

    exercicio-node-package-json

---
+++ modulo5/node-package-json/exercicio1.js
@@ -0,0 +1,10 @@
+// ----------------Exercício 1 -------
+// a) Para acessar os parâmetros passados na linha de comando do node, usa-se
+//  process.argv[2] (3,4,5...)
+
+ const nome = process.argv[2]
+ const idade =  Number(process.argv[3]) 
+ const novaIdade = idade + 7
+
+ console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}`)
