47bdf03e2157ebec5c7a13d3a1a68c53d7e3f5ef
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-06-26 17:00:00 -0300

    fim

--- modulo5/projeto-to-do-list/src/Router/routes.ts
+++ modulo5/projeto-to-do-list/src/Router/routes.ts
@@ -12,8 +12,8 @@ import {
 export const router = Router();
 
 router.get("/todoLists/all", readTodoListAllControllers)
-router.get("/todoLists/:id/task", readTodoListUserTaskControllers)
 router.get("/todoLists/:id/User", readTodoListUsersControllers)
+router.get("/todoLists/:id/task", readTodoListUserTaskControllers)
 router.post("/todoListUsers/create", createTodoListUsersControllers)
 router.post("/todoLists/task", createTodoListTaskControllers)
 router.put("/todoListsUser/:id/update", updateTodoLIstUsersControllers)
