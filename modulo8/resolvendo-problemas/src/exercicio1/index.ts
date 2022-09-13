0b9a59c0b15882e4362de3b278203b7e210d9639
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-09-13 16:26:18 -0300

    resolvendo-problemas

---
+++ modulo8/resolvendo-problemas/src/exercicio1/index.ts
@@ -0,0 +1,21 @@
+
+
+ function oneEdit (strA: string, strB: string): boolean {
+
+    if(Math.abs(strB.length - strA.length) > 1) return false;
+
+    if(strA.length > strB.length) return strA.includes(strB);
+
+    if(strB.length > strA.length) return strB.includes(strA);
+
+    let charsDiffCount = 0;
+
+    for(let i= 0; i < strA.length; i ++) {
+        if(strA[i] !== strB[i]) charsDiffCount++
+    }
+
+    return charsDiffCount === 1;
+}
+
+
+console.log(oneEdit("banana", "panana"));
---
+++ modulo8/resolvendo-problemas/src/exercicio2/index.ts
@@ -0,0 +1,6 @@
+
+
+function contaCaracterRepetido (str: string) {
+
+   
+}
